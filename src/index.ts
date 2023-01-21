b915008228f49e5c9d59fdbfe4a44c0c4c498bbb
akiraueno <akky@akky.me>
2023-01-22 00:40:51 +0900

    rm console.log

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,23 @@ const app = new Hono<{ Bindings: Env }>();
 
 const fileUploadHandler = async (c: Context) => {
   const fileName = c.req.param("file_name");
-  console.log("start");
   const startDate = Date.now();
+
   const body = await c.req.arrayBuffer();
   await c.env.BUCKET.put(fileName, body, {
     httpMetadata: { contentType: "model/gltf+json" },
   });
+
   const execTime = Date.now() - startDate;
+  console.log({ message: "ok", execTime: execTime });
   return c.json({ message: "ok", execTime: execTime });
 };
 
 const fileDownloadHandler = async (c: Context) => {
-  console.log("f1");
   const cacheKey = new Request(c.req.url.toString(), c.req);
   const cache = caches.default;
   const cachedRes = await cache.match(cacheKey);
-  console.log("f2");
+  // https://github.com/syumai/workers-playground/blob/b1cd8de5f4c4d9f1141fba420c96dcd644f70343/r2-image-viewer/src/index.ts
   if (cachedRes) {
     const etag = c.req.headers.get("If-None-Match");
     if (etag !== null && etag === cachedRes.headers.get("ETag")) {
@@ -34,15 +35,18 @@ const fileDownloadHandler = async (c: Context) => {
         headers: cachedRes.headers,
       });
     }
-    console.log("cache hit!!!!!!!!!!!");
+    console.log("cache hit");
     return cachedRes;
+  } else {
+    console.log("cache miss");
   }
 
   const fileName = c.req.param("file_name");
-  console.log("start");
   const startDate = Date.now();
+
   const object = await c.env.BUCKET.get(fileName);
   const data = await object.arrayBuffer();
+
   const execTime = Date.now() - startDate;
   console.log({ message: "ok", execTime: execTime });
 
